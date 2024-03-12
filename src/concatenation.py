3df1c5409fe036a9801753d531b9d3a3f4024057
wilmar <wilmar.sepulveda@correounivalle.edu.co>
2024-03-11 22:06:17 -0500

    add logs and config

--- src/concatenation.py
+++ src/concatenation.py
@@ -1,10 +1,23 @@
 import pandas as pd
 import glob
 import os
+from src.utils import load_config
+import logging
+import sys
+import argparse
 
-def csv_concatenation(input_path):
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    stream=sys.stderr,
+)
+logger = logging.getLogger("concatenation")
+
+def csv_concatenation(config):
+    logger.info('concatenando data')
+    input_path = config['concatenation']['input_path']
     all_files = glob.glob(os.path.join(input_path, "*.csv"))
-
     li = []
 
     for filename in all_files:
@@ -14,9 +27,9 @@ def csv_concatenation(input_path):
     frame = pd.concat(li, axis=0, ignore_index=True).drop_duplicates(subset='id', keep='last')
     return frame
 
-def delete_unused_files(input_path):
-
-
+def delete_unused_files(config):
+    logger.info('borrando archivos innecesarios')
+    input_path = config['concatenation']['input_path']
     # Get a list of all files in the directory
     files = glob.glob(input_path + "/*")
 
@@ -27,10 +40,19 @@ def delete_unused_files(input_path):
             # Delete the file
             os.remove(file)
 
+def main(config_path: str):
+    config = load_config(config_path)
+    output_path = config['concatenation']['output_path']
+    output_filename = output_path + "/" + config['concatenation']['output_filename']
+    final_data = csv_concatenation(config)
+    final_data.to_csv(output_filename)
+    delete_unused_files(config)
+
+
 
 # Llama a la función para descargar los archivos
 if __name__ == "__main__":
-    data_path = "data/raw"
-    final_data = csv_concatenation(data_path)
-    final_data.to_csv("data/raw/car_raw.csv")
-    delete_unused_files(data_path)
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument("--config", dest="config", required=True)
+    args = args_parser.parse_args()
+    main(args.config)
